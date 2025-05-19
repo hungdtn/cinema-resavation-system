de0e807892a47d319625e2030f9dd9a8c0da3b7a
Dinh Tri Nhat Hung <hungdtnph52072@gmail.com>
2025-05-19 09:35:15 +0700

    Initial commit - add Cinema Reservation System

---
+++ src/main/java/org/example/cinema_reservation_system/CinemaReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.cinema_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cinema_reservation_system/CinemaReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cinema_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
